392ae6292b1e88e346dade90670b0139bab659d7
Mariani88 <pm_mariani@hotmail.com>
2017-03-24 18:36:26 -0300

    refactor

--- proc-de-imagenes/src/main/java/untref/interfacebuilders/MenuBarBuilder.java
+++ proc-de-imagenes/src/main/java/untref/interfacebuilders/MenuBarBuilder.java
@@ -20,40 +20,48 @@ public class MenuBarBuilder {
 
 	public MenuBar build(final ImageView imageView) {
 		MenuBar menuBar = new MenuBar();
+		Menu fileMenu = createFileMenu(imageView);
+		menuBar.getMenus().addAll(fileMenu);
+		return menuBar;
+	}
+
+	private Menu createFileMenu(ImageView imageView) {
 		Menu fileMenu = new Menu("File");
+		MenuItem fileMenuItem = createOpenMenuItem(imageView);
+		fileMenu.getItems().addAll(fileMenuItem);
+		return fileMenu;
+	}
+
+	private MenuItem createOpenMenuItem(ImageView imageView) {
 		MenuItem fileMenuItem = new MenuItem("open...");
+		final FileChooser fileChooser = createOpenFileChooser();
+		setOpenEvent(imageView, fileMenuItem, fileChooser);
+		return fileMenuItem;
+	}
 
+	private FileChooser createOpenFileChooser() {
 		final FileChooser fileChooser = new FileChooser();
-
+		fileChooser.setTitle("Open image");
 		fileChooser.getExtensionFilters()
 				.addAll(new FileChooser.ExtensionFilter("All Images", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg"),
 						new FileChooser.ExtensionFilter("PNG", "*.png"), new FileChooser.ExtensionFilter("RAW", "*.raw"),
 						new FileChooser.ExtensionFilter("PGM", "*.pgm"), new FileChooser.ExtensionFilter("PPM", "*.ppm"),
 						new FileChooser.ExtensionFilter("BMP", "*.bmp"));
+		return fileChooser;
+	}
 
-		fileChooser.setTitle("Open Resource File");
+	private void setOpenEvent(final ImageView imageView, MenuItem fileMenuItem, final FileChooser fileChooser) {
 		fileMenuItem.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				File file = fileChooser.showOpenDialog(null);
-				if (file == null) {
-					System.out.println("file is null");
-				}
-
-				BufferedImage bufferedImage = null;
 				try {
-					bufferedImage = ImageIO.read(file);
+					BufferedImage bufferedImage = ImageIO.read(file);
+					Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+					imageView.setImage(image);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-
-				Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-				imageView.setImage(image);
-
 			}
 		});
-
-		fileMenu.getItems().addAll(fileMenuItem);
-		menuBar.getMenus().addAll(fileMenu, new Menu("hola"));
-		return menuBar;
 	}
 }
